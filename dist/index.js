58e14820aeec4fa73d7012a5fb460733f6dc64d2
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-12 19:25:59 -0300

    ORM - classe Cliente implementado

--- dist/index.js
+++ dist/index.js
@@ -18,10 +18,13 @@ class PessoaP extends model_1.Base {
         this.nome = '';
     }
 }
-class Cliente extends model_1.Base {
+class ClienteC extends model_1.Base {
     constructor() {
         super(...arguments);
-        this.nome_tabela = 'cliente';
+        this.nome_tabela = 'Cliente';
+        this.nome = '';
+        this.endereco = '';
+        this.email = '';
     }
 }
 (() => __awaiter(this, void 0, void 0, function* () {
@@ -29,12 +32,18 @@ class Cliente extends model_1.Base {
         host: '127.0.0.1',
         user: 'root',
         password: '',
-        database: 'teste',
+        database: 'whatsapp',
     });
     let p1 = new PessoaP();
     yield p1.findOne(2);
-    console.log('Meu nome é ', p1.nome + ' Email: ', p1.email);
-    // let c1 = new Cliente();
-    // c1.findOne(5);
+    console.log('Meu nome é ', p1.nome + ' / Email: ', p1.email);
+    let c1 = new ClienteC();
+    yield c1.findOne(1);
+    console.log('Cliente Nome: ' +
+        c1.nome +
+        ' / Endereço: ' +
+        c1.endereco +
+        ' / E-mail: ' +
+        c1.email);
     yield mysql_1.connectOFF();
 }))();
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,11 @@ class PessoaP extends Base {
   nome: string = '';
 }
 
-class Cliente extends Base {
-  nome_tabela: string = 'cliente';
+class ClienteC extends Base {
+  nome_tabela: string = 'Cliente';
+  nome: string = '';
+  endereco: string = '';
+  email: string = '';
 }
 
 (async () => {
@@ -16,15 +19,23 @@ class Cliente extends Base {
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'teste',
+    database: 'whatsapp',
   });
 
   let p1 = new PessoaP();
   await p1.findOne(2);
-  console.log('Meu nome é ', p1.nome + ' Email: ', p1.email);
+  console.log('Meu nome é ', p1.nome + ' / Email: ', p1.email);
 
-  // let c1 = new Cliente();
-  // c1.findOne(5);
+  let c1 = new ClienteC();
+  await c1.findOne(1);
+  console.log(
+    'Cliente Nome: ' +
+      c1.nome +
+      ' / Endereço: ' +
+      c1.endereco +
+      ' / E-mail: ' +
+      c1.email
+  );
 
   await connectOFF();
 })();
