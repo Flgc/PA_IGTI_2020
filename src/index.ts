50e16428c96637715d6ad64669ab1336d51e4d3b
Fabio Luis <ml@fabiotecnico1.biz>
2020-11-13 21:22:31 -0300

    Conexao com banco de dados MySql implementado

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import { Base } from './model';
 import { connectOn, connectOFF } from './dao/mysql';
 
 (async () => {
-  await connectOn();
+  await connectOn({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'whatsapp',
+  });
+
+  //
 
   await connectOFF();
 })();
