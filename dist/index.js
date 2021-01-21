4613dcd2b896548cedb734338bb4662f8e4b77b5
Fabio Luis <ml@fabiotecnico1.biz>
2021-01-20 23:53:54 -0300

    Venom-Bot Implementado

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,59 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const model_1 = require("./model");
 const mysql_1 = require("./dao/mysql");
-class PessoaP extends model_1.Base {
+const venom_bot_1 = require("venom-bot");
+class Lead extends model_1.Base {
     constructor() {
         super(...arguments);
-        this.nome_tabela = 'Pessoa';
-        this.email = '';
-        this.nome = '';
+        this.nome_tabela = 'lead';
+        this.name = '';
+        this.phone = '';
     }
 }
-class ClienteC extends model_1.Base {
-    constructor() {
-        super(...arguments);
-        this.nome_tabela = 'Cliente';
-        this.nome = '';
-        this.endereco = '';
-        this.email = '';
-    }
+function main(whots) {
+    whots.onMessage((message) => __awaiter(this, void 0, void 0, function* () {
+        if (String(message.body).toUpperCase() == 'SAIR') {
+            whots.sendText(message.from, 'Nossa conversa não será armazenadas, obrigado! ');
+            return;
+        }
+        if (String(message.body).toUpperCase() == 'SIM') {
+            let proprietario = new Lead();
+            proprietario.name = message.sender.pushname;
+            proprietario.phone = String(message.from).split('@')[0];
+            yield proprietario.populate;
+            whots.sendText(message.from, 'Obrigado por autorizar, registraremos na base de dados do histórico' +
+                ' do seu veículo.');
+            return;
+        }
+        whots.sendText(message.from, 'Olá!!! \n' +
+            'Sou uma atendente virtual. \n' +
+            'Com a sua autorização irei armazenar em uma base de dados nossas' +
+            ' conversas. \n\n' +
+            ' Digite "SIM" para autorizar ou "SAIR" para proceguir sem o armazenamento.');
+    }));
 }
-(() => __awaiter(this, void 0, void 0, function* () {
-    yield mysql_1.connectOn({
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'whatsapp',
-    });
-    let p1 = new PessoaP();
-    yield p1.findOne(2);
-    console.log('Meu nome é ', p1.nome + ' / Email: ', p1.email);
-    let c1 = new ClienteC();
-    yield c1.findOne(1);
-    console.log('Cliente Nome: ' +
-        c1.nome +
-        ' / Endereço: ' +
-        c1.endereco +
-        ' / E-mail: ' +
-        c1.email);
-    yield mysql_1.connectOFF();
-}))();
+try {
+    (() => __awaiter(void 0, void 0, void 0, function* () {
+        yield mysql_1.connectOn({
+            host: '127.0.0.1',
+            user: 'root',
+            password: '',
+            database: 'whatssap',
+        });
+        let venom = yield venom_bot_1.create();
+        main(venom);
+    }))();
+}
+catch (error) {
+    mysql_1.connectOFF();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,63 @@
 import { Base } from './model';
 import { connectOn, connectOFF } from './dao/mysql';
+import { create, Whatsapp } from 'venom-bot';
 
-class PessoaP extends Base {
-  nome_tabela: string = 'Pessoa';
-  email: string = '';
-  nome: string = '';
+class Lead extends Base {
+  nome_tabela: string = 'lead';
+  name: string = '';
+  phone: string = '';
 }
 
-class ClienteC extends Base {
-  nome_tabela: string = 'Cliente';
-  nome: string = '';
-  endereco: string = '';
-  email: string = '';
-}
+function main(whots: Whatsapp) {
+  whots.onMessage(async (message) => {
+    if (String(message.body).toUpperCase() == 'SAIR') {
+      whots.sendText(
+        message.from,
+        'Nossa conversa não será armazenadas, obrigado! '
+      );
 
-(async () => {
-  await connectOn({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'whatsapp',
+      return;
+    }
+
+    if (String(message.body).toUpperCase() == 'SIM') {
+      let proprietario = new Lead();
+      proprietario.name = message.sender.pushname;
+      proprietario.phone = String(message.from).split('@')[0];
+      await proprietario.populate;
+
+      whots.sendText(
+        message.from,
+        'Obrigado por autorizar, registraremos na base de dados do histórico' +
+          ' do seu veículo.'
+      );
+
+      return;
+    }
+
+    whots.sendText(
+      message.from,
+      'Olá!!! \n' +
+        'Sou uma atendente virtual. \n' +
+        'Com a sua autorização irei armazenar em uma base de dados nossas' +
+        ' conversas. \n\n' +
+        ' Digite "SIM" para autorizar ou "SAIR" para proceguir sem o armazenamento.'
+    );
   });
+}
 
-  let p1 = new PessoaP();
-  await p1.findOne(2);
-  console.log('Meu nome é ', p1.nome + ' / Email: ', p1.email);
+try {
+  (async () => {
+    await connectOn({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '',
+      database: 'whatssap',
+    });
 
-  let c1 = new ClienteC();
-  await c1.findOne(1);
-  console.log(
-    'Cliente Nome: ' +
-      c1.nome +
-      ' / Endereço: ' +
-      c1.endereco +
-      ' / E-mail: ' +
-      c1.email
-  );
+    let venom = await create();
 
-  await connectOFF();
-})();
+    main(venom);
+  })();
+} catch (error) {
+  connectOFF();
+}
